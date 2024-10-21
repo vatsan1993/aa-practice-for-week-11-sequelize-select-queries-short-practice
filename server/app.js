d0d1940619c5b64ade3266897c542c9943968e70
Srivatsan <vatsan1993@gmail.com>
2024-10-21 16:53:52 +0530

    finished project

--- server/app.js
+++ server/app.js
@@ -9,89 +9,110 @@ require('dotenv').config();
 const { Puppy } = require('./db/models');
 
 // Import Op to perform comparison operations in WHERE clauses - DO NOT MODIFY
-const { Op } = require("sequelize");
+const { Op, where } = require('sequelize');
 
 // Express using json - DO NOT MODIFY
 app.use(express.json());
-
 
 // STEP 1
 // All puppies in the database
 // No WHERE clause
 app.get('/puppies', async (req, res, next) => {
-    let allPuppies;
+  let allPuppies;
 
-    // Your code here
-
-    res.json(allPuppies);
+  // Your code here
+  allPuppies = await Puppy.findAll({ order: ['name'] });
+  res.json(allPuppies);
 });
-
 
 // STEP 2
 // All puppies that have been microchipped
 // WHERE clause with one exact value
 app.get('/puppies/chipped', async (req, res, next) => {
-    let chippedPuppies;
+  let chippedPuppies;
 
-    // Your code here
+  // Your code here
+  chippedPuppies = await Puppy.findAll({
+    where: {
+      microChipped: true,
+    },
+  });
 
-    res.json(chippedPuppies);
+  res.json(chippedPuppies);
 });
-
 
 // STEP 3
 // One puppy matching a name param
 // Finding one record by attribute
 app.get('/puppies/name/:name', async (req, res, next) => {
-    let puppyByName;
-    
-    // Your code here
+  let puppyByName;
 
-    res.json(puppyByName);
-})
+  // Your code here
+  let name = req.params.name;
+  puppyByName = Puppy.findOne({
+    where: {
+      name,
+    },
+  });
 
+  res.json(puppyByName);
+});
 
 // BONUS STEP 5
 // All puppies with breed ending in 'Shepherd'
 // WHERE clause with a comparison
 app.get('/puppies/shepherds', async (req, res, next) => {
-    let shepherds;
-    
-    // Your code here
+  let shepherds;
 
-    res.json(shepherds);
-})
+  // Your code here
 
+  shepherds = await Puppy.findAll({
+    where: {
+      breed: {
+        [Op.like]: `%Shepherd`,
+      },
+    },
+  });
+  res.json(shepherds);
+});
 
 // BONUS STEP 6
 // All puppies with ageYrs <= 1yr and weightLbs <= 20lbs
 // WHERE clause with multiple attributes and comparisons
 app.get('/puppies/tinybabies', async (req, res, next) => {
-    let tinyBabyPuppies;
-    
-    // Your code here
+  let tinyBabyPuppies;
 
-    res.json(tinyBabyPuppies);
-})
+  // Your code here
+  tinyBabyPuppies = await Puppy.findAll({
+    where: {
+      [Op.and]: {
+        ageYrs: { [Op.lte]: 1 },
+        weightLbs: { [Op.lte]: 20 },
+      },
+    },
+  });
 
+  res.json(tinyBabyPuppies);
+});
 
 // STEP 4
 // One puppy matching an id param
 // Finding one record by primary key
 app.get('/puppies/:id', async (req, res, next) => {
-    let puppyById;
-    
-    // Your code here
-    
-    res.json(puppyById);
-});
+  let puppyById;
 
+  // Your code here
+  let puppyId = req.params.id;
+  puppyById = await Puppy.findByPk(puppyId);
+
+  res.json(puppyById);
+});
 
 // Root route - DO NOT MODIFY
 app.get('/', (req, res) => {
-    res.json({
-        message: "API server is running"
-    });
+  res.json({
+    message: 'API server is running',
+  });
 });
 
 // Set port and listen for incoming requests - DO NOT MODIFY
